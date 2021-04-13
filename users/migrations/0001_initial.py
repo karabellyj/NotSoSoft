84da262487613d02f817cc644f78f21cc6e13ca3
Jozef Karabelly <jozefkarabelly@gmail.com>
2021-04-13 19:50:44 +0200

    add CustomUser model

--- users/migrations/0001_initial.py
+++ users/migrations/0001_initial.py
@@ -10,95 +10,34 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('auth', '0012_alter_user_first_name_max_length'),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='Company',
+            name='CustomUser',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=255)),
-                ('logo', models.BinaryField()),
-                ('description', models.CharField(max_length=255, null=True)),
+                ('password', models.CharField(max_length=128, verbose_name='password')),
+                ('last_login', models.DateTimeField(blank=True, null=True, verbose_name='last login')),
+                ('is_superuser', models.BooleanField(default=False, help_text='Designates that this user has all permissions without explicitly assigning them.', verbose_name='superuser status')),
+                ('username', models.CharField(error_messages={'unique': 'A user with that username already exists.'}, help_text='Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.', max_length=150, unique=True, validators=[django.contrib.auth.validators.UnicodeUsernameValidator()], verbose_name='username')),
+                ('first_name', models.CharField(blank=True, max_length=150, verbose_name='first name')),
+                ('last_name', models.CharField(blank=True, max_length=150, verbose_name='last name')),
+                ('email', models.EmailField(blank=True, max_length=254, verbose_name='email address')),
+                ('is_staff', models.BooleanField(default=False, help_text='Designates whether the user can log into this admin site.', verbose_name='staff status')),
+                ('is_active', models.BooleanField(default=True, help_text='Designates whether this user should be treated as active. Unselect this instead of deleting accounts.', verbose_name='active')),
+                ('date_joined', models.DateTimeField(default=django.utils.timezone.now, verbose_name='date joined')),
+                ('groups', models.ManyToManyField(blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', related_name='user_set', related_query_name='user', to='auth.Group', verbose_name='groups')),
+                ('user_permissions', models.ManyToManyField(blank=True, help_text='Specific permissions for this user.', related_name='user_set', related_query_name='user', to='auth.Permission', verbose_name='user permissions')),
             ],
-        ),
-        migrations.CreateModel(
-            name='Project',
-            fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=255)),
-                ('description', models.CharField(max_length=3000)),
-                ('start_date', models.DateField()),
-                ('real_end', models.DateField(null=True)),
-                ('estimated_end_date', models.DateField(null=True)),
-                ('company', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='users.company')),
-                ('project_manager', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
-            ],
-        ),
-        migrations.CreateModel(
-            name='ProjectPhase',
-            fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('phase_manager', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
-                ('project', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='users.project')),
-            ],
-        ),
-        migrations.CreateModel(
-            name='RiskImpact',
-            fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('impacts', models.CharField(max_length=60)),
-            ],
-        ),
-        migrations.CreateModel(
-            name='RiskProbability',
-            fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('probability', models.CharField(max_length=60)),
-            ],
-        ),
-        migrations.CreateModel(
-            name='RiskState',
-            fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('state', models.CharField(max_length=60)),
-            ],
-        ),
-        migrations.CreateModel(
-            name='RiskType',
-            fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('type', models.CharField(max_length=255)),
-            ],
-        ),
-        migrations.CreateModel(
-            name='RiskSubtype',
-            fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('subtype', models.CharField(max_length=255)),
-                ('risk_type', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='users.risktype')),
-            ],
-        ),
-        migrations.CreateModel(
-            name='Risk',
-            fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=255)),
-                ('description', models.CharField(max_length=3000, null=True)),
-                ('threat', models.CharField(max_length=1000)),
-                ('starter', models.CharField(max_length=1000)),
-                ('reactions', models.CharField(max_length=1000, null=True)),
-                ('comment', models.CharField(max_length=1000, null=True)),
-                ('start_date', models.DateField()),
-                ('state_change_date', models.DateField(null=True)),
-                ('reaction_date', models.DateField(null=True)),
-                ('project_phase', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='users.projectphase')),
-                ('risk_assignee', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
-                ('risk_impacts', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='users.riskimpact')),
-                ('risk_probability', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='users.riskprobability')),
-                ('risk_state', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='users.riskstate')),
-                ('risk_type', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='users.risktype')),
+            options={
+                'verbose_name': 'user',
+                'verbose_name_plural': 'users',
+                'abstract': False,
+            },
+            managers=[
+                ('objects', django.contrib.auth.models.UserManager()),
             ],
         ),
     ]
