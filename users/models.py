cb068954e167681aa4970d265ef5e55d84194014
Jozef Karabelly <jozefkarabelly@gmail.com>
2021-04-13 19:50:44 +0200

    add CustomUser model

--- users/models.py
+++ users/models.py
@@ -1,1 +1,8 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+
+from risk.models import Company
+
+
+class CustomUser(AbstractUser):
+    company = models.ForeignKey(Company, on_delete=models.SET_NULL, null=True)
