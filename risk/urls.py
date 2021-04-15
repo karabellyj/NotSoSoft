fb3b8bf7539b1e9060dcb55aabca927fa6d4f607
Jozef Karabelly <jozefkarabelly@gmail.com>
2021-04-15 15:49:25 +0200

    add risk register view

--- risk/urls.py
+++ risk/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import (ProjectCreateView, ProjectDetailView, ProjectListView,
                     ProjectPhaseCreateView, ProjectPhaseDetailView,
                     ProjectPhaseUpdateView, ProjectUpdateView, RiskCreateView,
-                    RiskDetailView, RiskUpdateView)
+                    RiskDetailView, RiskListView, RiskUpdateView)
 
 urlpatterns = [
     path('projects/', ProjectListView.as_view(), name='project-list'),
@@ -11,11 +11,13 @@ urlpatterns = [
     path('project/<int:pk>/update/', ProjectUpdateView.as_view(), name='project-update'),
     path('project/<int:pk>/', ProjectDetailView.as_view(), name='project-detail'),
 
-    path('project/<int:project_id>/create/', ProjectPhaseCreateView.as_view(), name='project-phase-create'),
-    path('project/phase/<int:pk>/update/', ProjectPhaseUpdateView.as_view(), name='project-phase-update'),
-    path('project/phase/<int:pk>/', ProjectPhaseDetailView.as_view(), name='project-phase-detail'),
+    path('project/<int:project_id>/phase/create/', ProjectPhaseCreateView.as_view(), name='project-phase-create'),
+    path('project/<int:project_id>/phase/<int:pk>/update/', ProjectPhaseUpdateView.as_view(), name='project-phase-update'),
+    path('project/<int:project_id>/phase/<int:pk>/', ProjectPhaseDetailView.as_view(), name='project-phase-detail'),
 
-    path('project/phase/<int:phase_id>/create/', RiskCreateView.as_view(), name='risk-create'),
-    path('project/phase/risk/<int:pk>/update', RiskUpdateView.as_view(), name='risk-update'),
-    path('project/phase/<int:pk>/', RiskDetailView.as_view(), name='risk-detail')
+    path('project/<int:project_id>/phase/<int:phase_id>/risk/create/', RiskCreateView.as_view(), name='risk-create'),
+    path('project/<int:project_id>/phase/<int:phase_id>/risk/<int:pk>/update', RiskUpdateView.as_view(), name='risk-update'),
+    path('project/<int:project_id>/phase/<int:phase_id>/risk/<int:pk>/', RiskDetailView.as_view(), name='risk-detail'),
+
+    path('project/<int:project_id>/register/', RiskListView.as_view(), name='risk-register')
 ]
